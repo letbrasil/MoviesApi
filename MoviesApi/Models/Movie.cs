0a3368a24a2eaff9f5e7457e716ba3ab3fba5d84
Letícia Brasileiro <CS374170@MinhaTI.com.br>
2023-11-11 23:59:14 -0300

    first commit

---
+++ MoviesApi/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [StringLength(50, ErrorMessage = "O gênero não pode exceder 50 caracteres")]
+        public string Genre { get; set; }
+
+        [Required]
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duration { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+    }
+}
